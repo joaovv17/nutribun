2cf902a0a765d7b46919d34ae4d20f88c5ac93e5
joaovv17 <joaovibarboa@gmail.com>
2025-10-02 09:02:03 -0300

    Add files via upload

--- f.js
+++ f.js
@@ -47,3 +47,32 @@ titulo.addEventListener('click', mostraMensagem);
 function mostraMensagem(){
     alert("Este elemento foi clicado");
 }
+//acessa o botao
+var botaoAdicionar = document.querySelector("#adicionar-paciente");
+//executa o codigo ao acionar o botao
+botaoAdicionar.addEventListener('click', function(event){
+event.preventDefault();
+
+//acessa o formulario\\
+var formulario = document.querySelector("#form-adiciona");
+
+var nome = formulario.nome.value;
+var peso = formulario.peso.value;
+var altura = formulario.altura.value;
+var gordura = formulario.gordura.value;
+
+//é pra criar a tag tr da tabela 
+var pacienteTr = document.createElement("tr");
+//é pra criar a tag td da tabela 
+var nomeTd = document.createElement("td");
+var pesoTd = document.createElement("td");
+var alturaTd = document.createElement("td");
+var gorduraTd = document.createElement("td");
+var imcTd = document.createElement("td");
+//adiciona os valores
+nomeTd.textContent = nome;
+pesoTd.textContent = peso;
+alturaTd.textContent = altura;
+gorduraTd.textContent = gordura;
+imcTd.textContent = imc;
+});
